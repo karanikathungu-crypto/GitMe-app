192b714165d85c0cd2d1430e16fdffb955b51565
karanikathungu-crypto <kathungudev@gmail.com>
2021-08-29 23:39:00 +0300

    Add the divs for display of github user

--- GitMe-app/GitMe/src/app/user-profile/user-profile.component.ts
+++ GitMe-app/GitMe/src/app/user-profile/user-profile.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-user-profile',
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserProfileComponent implements OnInit {
 
+  @Input() username:any;
   constructor() { }
 
   ngOnInit(): void {
