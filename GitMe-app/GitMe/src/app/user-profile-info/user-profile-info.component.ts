0dfe0a06ee7760a0a4993ad26fc583d760db35c6
karanikathungu-crypto <kathungudev@gmail.com>
2021-08-30 01:12:38 +0300

    Add the badges for profile info

--- GitMe-app/GitMe/src/app/user-profile-info/user-profile-info.component.ts
+++ GitMe-app/GitMe/src/app/user-profile-info/user-profile-info.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-user-profile-info',
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-profile-info.component.css']
 })
 export class UserProfileInfoComponent implements OnInit {
-
+  @Input() username:any;
   constructor() { }
 
   ngOnInit(): void {
