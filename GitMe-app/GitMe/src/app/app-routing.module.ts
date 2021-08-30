0e9595e431378daf8ce49898627ab4cfc857c777
karanikathungu-crypto <kathungudev@gmail.com>
2021-08-30 10:44:30 +0300

    Add routes for navigation

--- GitMe-app/GitMe/src/app/app-routing.module.ts
+++ GitMe-app/GitMe/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactComponent } from './contact/contact.component';
+import { DisplayFormComponent } from './display-form/display-form.component';
+import { NavDisplayComponent } from './nav-display/nav-display.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'contacts' ,component: ContactComponent},
+  {path:'home', component: DisplayFormComponent},
+  {path:'navbar', component: NavDisplayComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
